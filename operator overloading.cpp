f7df4342a206c5877695d5b9a2bc456cfdb66d81
gagan005 <168338988+prajapatigagan@users.noreply.github.com>
2025-09-12 22:48:42 +0530

    Update operator overloading.cpp

--- operator overloading.cpp
+++ operator overloading.cpp
@@ -1,60 +1,45 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
-class num
-{
-	private:
-		int awa;
-		int ama;
-	public:
-		num()
-		{
-			awa=0;
-			ama=0;
-		}
-	 
-    num(int a,int b)
-	{ 
-	awa=a;
-	ama=b;                                                                            
-	}
-	int show()
-	{
-		cout<<awa<<" first "<<ama<<" second "<<endl;
-	}
-	num operator +(num A1)//binary operator ;
-	{
-		num temp;
-		temp.awa=awa+A1.awa;
-		temp.ama=ama+A1.ama;
-		return temp;
-	}
-	
-		
+
+class num {
+private:
+    int awa;
+    int ama;
+
+public:
+    // Default constructor
+    num() {
+        awa = 0;
+        ama = 0;
+    }
+
+    // Parameterized constructor
+    num(int a, int b) {
+        awa = a;
+        ama = b;
+    }
+
+    // Function to display values
+    void show() {
+        cout << awa << " first " << ama << " second " << endl;
+    }
+
+    // Operator overloading (+)
+    num operator+(num A1) {
+        num temp;
+        temp.awa = awa + A1.awa;
+        temp.ama = ama + A1.ama;
+        return temp;
+    }
 };
-int main()
-{
-	num a1(7,8);
-	num b1(7,4);
-	num c1;
-	
-	c1=a1+b1; //C1=a1.add(b1)
-	c1.show();
-}
 
+int main() {
+    num a1(7, 8);
+    num b1(7, 4);
+    num c1;
 
+    c1 = a1 + b1;  // Operator overloading
+    c1.show();
 
-//function overloading;
-int num(int a,int b)
-{
-	cout<<a+b<<endl;
-}
-int add(double a,double b)
-{
-	cout<<a+b;
-}
-int main()
-{
-	num(3,66);
-	add(6,8);
+    return 0;
 }
